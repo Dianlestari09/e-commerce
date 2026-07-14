581e81e2647b8d18f535217b80c0826ce198c1c8
Dianlestari09 <230605110016@student.uin-malang.ac.id>
2026-07-14 08:54:03 +0700

    chore: remove .vercel untracked files and update gitignore

--- .vercel/output/_functions/chunks/supabase_CciFyEBF.mjs
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-var supabase = createClient("https://ykdzmonuxadattbfaxdw.supabase.co", "sb_publishable_YsWL7FrjY4VKzV82vSLjug_-LTCBWJr");
-//#endregion
-export { supabase as t };
